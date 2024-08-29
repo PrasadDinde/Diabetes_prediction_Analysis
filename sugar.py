69b0122711a2684e5cc41506625de73e0eb1af79
Prasad Rahul Dinde <94909430+PrasadDinde@users.noreply.github.com>
2024-08-29 17:15:28 +0530

    sugar.py

--- sugar.py
+++ sugar.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import sklearn
 import os
 
 with open('Machine Learning/Supervised learning/Project_Predictive_Analysis_Diabetes/classification_model.pkl', 'rb') as f:
